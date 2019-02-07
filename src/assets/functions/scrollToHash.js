97c85e46989e005d72975b379082f32891a5c334
Simon Cooper <kidney9@hotmail.com>
2019-02-07 17:04:53 +0000

    Smooth scrolling only enabled on Firefox to fix bug

--- src/assets/functions/scrollToHash.js
+++ src/assets/functions/scrollToHash.js
@@ -5,7 +5,11 @@ export default (href) => {
     const targetSelectorArr = href && (href.match(hashRegex) || ['body']) || window.location.href.match(hashRegex);
     const targetElement = targetSelectorArr ? document.querySelector(targetSelectorArr[0]) : null;
     if (!targetElement) return;
+    const { userAgent } = window.navigator;
+    const firefoxRE = /Firefox\/(\d*\.?\d*)/i;
+    const scrollParams = userAgent.match(firefoxRE) && +userAgent.match(firefoxRE)[1] >= 64 ? { behavior: 'smooth' } : true;
     // timeOut must be large enough to allow full page loading and therefore correct scrolling
     const timeOut = getRelativePathNoHash(href) === getRelativePathNoHash(window.location.pathname + window.location.hash) ? 0 : 500;
-    setTimeout(() => targetElement.scrollIntoView({ behavior: 'smooth' }), timeOut);
+    // only use the experimental options object on scrollIntoView if on Firefox >= 64 (doesn't work accurately on Chrome 72)
+    setTimeout(() => targetElement.scrollIntoView(scrollParams), timeOut);
 }

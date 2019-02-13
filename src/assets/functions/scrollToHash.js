3543c77409c0e6170254e8c783c224b0afe93d8b
Simon Cooper <kidney9@hotmail.com>
2019-02-13 18:43:33 +0000

    Scroll behavior now declared in css rather than javascript

--- src/assets/functions/scrollToHash.js
+++ src/assets/functions/scrollToHash.js
@@ -5,11 +5,7 @@ export default (href) => {
     const targetSelectorArr = href && (href.match(hashRegex) || ['body']) || window.location.href.match(hashRegex);
     const targetElement = targetSelectorArr ? document.querySelector(targetSelectorArr[0]) : null;
     if (!targetElement) return;
-    const { userAgent } = window.navigator;
-    const firefoxRE = /Firefox\/(\d*\.?\d*)/i;
-    const scrollParams = userAgent.match(firefoxRE) && +userAgent.match(firefoxRE)[1] >= 64 ? { behavior: 'smooth' } : true;
     // timeOut must be large enough to allow full page loading and therefore correct scrolling
     const timeOut = getRelativePathNoHash(href) === getRelativePathNoHash(window.location.pathname + window.location.hash) ? 0 : 500;
-    // only use the experimental options object on scrollIntoView if on Firefox >= 64 (doesn't work accurately on Chrome 72)
-    setTimeout(() => targetElement.scrollIntoView(scrollParams), timeOut);
+    setTimeout(() => targetElement.scrollIntoView(), timeOut);
 }
